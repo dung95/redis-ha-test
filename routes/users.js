d4c996c4bba8b571320562f1fe79dbeb3e356dc0
Nguyen Tan Dung <dungtan95er@gmail.com>
2018-07-25 23:02:56 +0700

    updatessss

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var Redis = require('ioredis');
 var redis = new Redis({
-  sentinels: [{ host: '35.199.169.52', port: 26379 }],
+  sentinels: [{ host: 'rfs-dev', port: 26379 }],
   name: 'mymaster'
 });
 
 router.get('/', function(req, res, next) {
     let value = redis.zrange('foo', 0, 100, 'WITHSCORES');
     console.log(value);
+    res.send(value);
 });
 
 module.exports = router;
