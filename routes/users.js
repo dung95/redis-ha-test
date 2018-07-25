bcae8e1f3c56edbec078820b07f537e1b794a69f
Nguyen Tan Dung <dungtan95er@gmail.com>
2018-07-25 22:57:12 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,13 @@ var express = require('express');
 var router = express.Router();
 var Redis = require('ioredis');
 var redis = new Redis({
-  sentinels: [{ host: 'rfs-redisfailover', port: 26379 }],
+  sentinels: [{ host: '35.199.169.52', port: 26379 }],
   name: 'mymaster'
 });
 
-router.post('/', function(req, res, next) {
- 
-  redis.set('username', req.body.username);
-  redis.get('username', function (err, result) {
-  res.send(result);
-  console.log(result);
-});
+router.get('/', function(req, res, next) {
+    let value = redis.zrange('foo', 0, 100, 'WITHSCORES');
+    console.log(value);
 });
 
 module.exports = router;
